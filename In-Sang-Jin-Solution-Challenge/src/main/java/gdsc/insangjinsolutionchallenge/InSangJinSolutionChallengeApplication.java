048e8e8fa04edc2fe8c70434a34869b678e7a6d2
In, Sangjin <insangjin6439@gmail.com>
2023-12-19 23:17:28 +0900

    make project

---
+++ In-Sang-Jin-Solution-Challenge/src/main/java/gdsc/insangjinsolutionchallenge/InSangJinSolutionChallengeApplication.java
@@ -0,0 +1,13 @@
+package gdsc.insangjinsolutionchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InSangJinSolutionChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InSangJinSolutionChallengeApplication.class, args);
+    }
+
+}
