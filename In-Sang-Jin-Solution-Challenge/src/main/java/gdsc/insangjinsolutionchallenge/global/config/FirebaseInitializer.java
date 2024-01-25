61900de696a32dc316f1fb1f3f1abe0aaf6b70cb
In, Sangjin <insangjin6439@gmail.com>
2024-01-26 00:29:32 +0900

    json파일 환경변수로 설정

--- In-Sang-Jin-Solution-Challenge/src/main/java/gdsc/insangjinsolutionchallenge/global/config/FirebaseInitializer.java
+++ In-Sang-Jin-Solution-Challenge/src/main/java/gdsc/insangjinsolutionchallenge/global/config/FirebaseInitializer.java
@@ -7,33 +7,38 @@ import com.google.firebase.auth.FirebaseAuth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseInitializer {
 
-    @Value("${firebase.json-file-path}")
-    private String jsonFilePath;
+    @Value("${firebase.private_key}")
+    private String privateKey;
+
+//    @Value("${firebase.json-file-path}")
+//    private String jsonFilePath;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
             // 클래스패스 상의 리소스를 로드
-            Resource resource = new ClassPathResource(jsonFilePath);
+//            Resource resource = new ClassPathResource(jsonFilePath);
+//
+//            // 리소스에서 InputStream을 얻음
+//            try (InputStream fis = resource.getInputStream()) {
+//                FirebaseOptions options = FirebaseOptions.builder()
+//                        .setCredentials(GoogleCredentials.fromStream(fis))
+//                        .build();
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ByteArrayInputStream(privateKey.getBytes()));
 
-            // 리소스에서 InputStream을 얻음
-            try (InputStream fis = resource.getInputStream()) {
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(fis))
-                        .build();
-
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(googleCredentials)
+                    .build();
                 return FirebaseApp.initializeApp(options);
             }
-        }
+
         return FirebaseApp.getInstance();
     }
 
